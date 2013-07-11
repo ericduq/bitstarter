5e8d34a2bd28a06b77f02f978d0d1b1ee81fea2d
Eric Duquette <ericduq@gmail.com>
2013-07-11 02:27:28 +0000

    Added fs.readFile and .toString and require(fs)

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  	var hello=fs.readFile('index.html')
+  	var fs=require('fs');
+	var hello=fs.readFile('index.html');
 	response.send(hello.toString);
 });
 
