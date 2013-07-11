bae674e731c67db7bf2fdde9f38c51c59ebfbb74
Eric Duquette <ericduq@gmail.com>
2013-07-11 02:48:51 +0000

    Added fs.readFile and .toString and require(fs) and new Buff

--- web.js
+++ web.js
@@ -4,8 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   	var fs=require('fs');
-	var hello= fs.readFileSync('index.html','utf-8');
-	response.send(hello);
+	response.send(fs.readFileSync('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
