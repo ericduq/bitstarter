d278c1437168a8e5b6c7dc68b023ab0906965474
Eric Duquette <ericduq@gmail.com>
2013-07-12 13:37:32 +0000

    Grader updated to check a url by suboptimally creating a temp file.

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLPATH_DEFAULT ="http://www.google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +68,54 @@ if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-u, --url <url>', 'Path to url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+
+	
+	var f_option=false;
+	var u_option=false;
+
+	for(var i=0; i<process.argv.length; i++){
+		if(process.argv[i]=="-f"){ f_option=true;}
+		if(process.argv[i]=="-u"){ u_option=true;}
+	}
+
+	var target='file';
+
+	if (f_option && u_option){
+		console.log("Cannot have both --file and --url");
+		process.exit(1);
+	}
+
+	if (u_option && program.url){
+		rest.get(program.url).on('complete',function(result){
+			if (result instanceof Error){
+				util.puts('Error: ' + result.message);
+				process.exit(1);
+			}
+			else{
+				var fs = require('fs');
+				fs.writeFile(".tmp.html",result, function(err){
+					if(err){
+						console.log(err)
+					}
+					else{
+						var checkJson=checkHtmlFile(".tmp.html",program.checks);
+						var outJson=JSON.stringify(checkJson,null,4);
+						console.log(outJson);
+					}
+				});
+			}
+		});
+		return;	
+	}
+	else{
+
+    		var checkJson = checkHtmlFile(program.file, program.checks);
+    		var outJson = JSON.stringify(checkJson, null, 4);
+    		console.log(outJson);
+	}
+}
+else {
     exports.checkHtmlFile = checkHtmlFile;
 }
